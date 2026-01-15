cb690460f936c98a18a81c168541f04a2306be7d
limzishen <limzishen27@gmail.com>
2026-01-15 16:52:14 +0800

    Implemented Level-0 Rename, Greet, Exit

---
+++ src/main/java/Dicky.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Dicky {
+    public static void main(String[] args) {
+        String line = "------------------------------";
+
+        //greet
+        String welcomeMessage = "Hello I'm Dicky";
+        System.out.println(line);
+        System.out.println(welcomeMessage);
+        System.out.println(line);
+
+        // Exit
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(exitMessage);
+        System.out.println(line);
+    }
+}
